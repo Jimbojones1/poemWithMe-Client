ec6aaa711989d786b6b2ea47d939e15f37e6a59d
Jim Haff <Jimmyhaff3@gmail.com>
2018-04-16 22:13:15 -0500

    added node packages

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { routerMiddleware } from 'react-router-redux';
+
+// Redux thunk is middleware for Redux that allows you to
+// write action creators that return a function instead of an action
+import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
+import rootReducer from './modules';
+import { composeWithDevToools } from 'redux-devtools-extension';
