da4858b443bf2f30a8d6ff9cfe19e598cd0171cb
Jim Haff <Jimmyhaff3@gmail.com>
2018-04-16 22:30:57 -0500

    setup react router

--- src/store.js
+++ src/store.js
@@ -1,8 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
-import { routerMiddleware } from 'react-router-redux';
-
 // Redux thunk is middleware for Redux that allows you to
 // write action creators that return a function instead of an action
 import thunk from 'redux-thunk';
